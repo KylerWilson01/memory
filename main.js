deb58d3335d46d7efddaefd0263575803be33f7e
Kyler Wilson <kyleraw2001@gmail.com>
2020-02-14 13:15:22 -0800

    functions correctly

--- main.js
+++ main.js
@@ -13,22 +13,41 @@ $(document).ready(function () {
 
     value = $(this).find('+ .back').html()
 
+    $(this).parent().addClass('flipped')
+
     if (num1 === '') {
       num1 += value
-    } else {
+    } else if (num2 === '') {
       num2 += value
     }
     console.log(num1, num2)
+    setTimeout(() => {
+      if (num1 === num2 && num1 !== '' && num2 !== '') {
 
-    if (num1 === num2) {
-      num1 = ''
-      num2 = ''
-      console.log('yay!')
-    } else if (num1 !== num2 && num1 !== '' && num2 !== '') {
-      num1 = ''
-      num2 = ''
-      console.log('grr!')
-    }
+        $(this).parent().removeClass('flipped').addClass('match')
+        $(this).parent().siblings().find('+ .flipped').removeClass('flipped').addClass('match')
+
+
+        $('.match').off('.flip')
+
+        num1 = ''
+        num2 = ''
+      } else if (num1 !== num2 && num1 !== '' && num2 !== '') {
+        setTimeout(() => {
+          $('.flipped').flip(false)
+        }, 500)
+
+        setTimeout(() => {
+          $(this).parent().siblings().removeClass('flipped')
+          $(this).parent().removeClass('flipped')
+        }, 600)
+
+
+        num1 = ''
+        num2 = ''
+      }
+    }, 100)
+
   })
 })
 
