0cde85e6bbcb76cbdce247df27c587c5062ff5fa
Kyler Wilson <kyleraw2001@gmail.com>
2020-02-15 20:14:37 -0800

    lose and win screen

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
+
   var memory = new Memory()
   memory.newGame()
   memory.log()
   memory.deal()
   $('.cards').flip()
 
+
   var num1 = ''
   var num2 = ''
+
+  let score = 0
+  let lives = 10
+  $('#lives').html(`You have ${lives} lives remaining`)
+  $('#score').html(`You have ${score} pairs flipped`)
 
   $('#board').on('click', '.front', function (e) {
     e.preventDefault()
@@ -20,30 +27,45 @@ $(document).ready(function () {
     } else if (num2 === '') {
       num2 += value
     }
-    console.log(num1, num2)
+
+    setTimeout(() => {
+      if (score === 9 && lives > 0) {
+        $('#game').html(`You Win!!! <button id='reset'> Start New Game</button>`)
+      } else if (lives <= 0) {
+        $('#game').html(`You Lost :( Try again <button id='reset'> Start New Game</button>`)
+      }
+    }, 1000)
+
     setTimeout(() => {
       if (num1 === num2 && num1 !== '' && num2 !== '') {
-        $(this).parent().removeClass('flipped').addClass('match')
-        $(this).parent().siblings().find('+ .flipped').removeClass('flipped').addClass('match')
-
+        $(this).parent().siblings().find('+ .flipped').addClass('match').removeClass('flipped')
+        $(this).parent().addClass('match').removeClass('flipped')
         $('.match').off('.flip')
+
+        score++
+        console.log(score, lives)
+        $('#lives').html(`You have ${lives} lives remaining`)
+        $('#score').html(`You have ${score} pairs flipped`)
 
         num1 = ''
         num2 = ''
       } else if (num1 !== num2 && num1 !== '' && num2 !== '') {
-        setTimeout(() => {
-          $('.flipped').flip(false)
-        }, 500)
+        $('.flipped').flip(false)
 
         setTimeout(() => {
           $(this).parent().siblings().removeClass('flipped')
           $(this).parent().removeClass('flipped')
-        }, 600)
+        }, 0)
+
+        lives--
+        console.log(score, lives)
+        $('#lives').html(`You have ${lives} lives remaining`)
+        $('#score').html(`You have ${score} pairs flipped`)
 
         num1 = ''
         num2 = ''
       }
-    }, 100)
+    }, 500)
 
   })
 })
